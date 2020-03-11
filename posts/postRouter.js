6cdf1a0d2ff59a9b72340b5fefb7e6093edce456
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-03-11 15:50:34 -0500

    MVP and Stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,60 @@
-const express = require('express');
-
+const express = require("express");
+const postDb = require("./postDb");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+//Get from '/api/posts'
+router.get("/", (req, res) => {
+   postDb.get() 
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error trying to get post",
+        err
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+//Get from '/api/posts/:id' for specific ID must pass ValidatePostID
+router.get("/:id", validatePostID, (req, res) => {
+  postDb.getById(req.params.id).then(post => {
+    post
+      ? res.status(200).json(req.post)
+      : res.status(404).json({ message: "No posts for this ID" });
+  });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+//Delete from 'api/posts/:id for specific ID must pass ValidatePostID
+router.delete("/:id", (req, res) => {
+  postDb.remove(req.params.id).then(post => {
+    post
+      ? res.status(200).json({ message: "This post has been destroyed" })
+      : res.status(400).json({
+          message: "This post doesn't exist or cannot be removed"
+        });
+  });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+//Updates from 'api/posts/:id for specific ID must pass ValidatePostID
+router.put("/:id", validatePostID, (req, res) => {
+  postDb.update(req.params.id).then(post => {
+    post
+      ? res.status(200).json(post)
+      : res
+          .status(404)
+          .json({ message: "The post with this ID could not be found" });
+  });
 });
 
-// custom middleware
-
-function validatePostId(req, res, next) {
-  // do your magic!
+//ValidatePostId custom middleware
+function validatePostID(req, res, next) {
+  postDb.getById(req.params.id).then(post => {
+    post
+      ? res.status(200).json({ message: "post ID is valid" })
+      : res.status(400).json({ message: "post ID is invalid" });
+  });
+  next();
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,127 @@
-const express = require('express');
-
+const express = require("express");
+const userDb = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+//Post to '/api/users'
+router.post("/", validateUser, (req, res) => {
+  userDb
+    .insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error adding user"
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+//Post to 'api/users' for a specific ID must pass Validation
+router.post("/:id/posts", validateUserId, (req, res) => {
+  userDb
+    .insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Could not add post for user"
+      });
+    });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+// Get users from '/api/users'
+router.get("/", validateUserId, (req, res) => {
+  userDb
+    .get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error getting users"
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+// Get user by id from '/api/users' validate
+router.get("/:id", validateUserId, (req, res) => {
+  userDb
+    .getById()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error finding user"
+      });
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+// Get all posts for specific user by id \\validate
+router.get("/:id/posts", validateUserId, (req, res) => {
+  userDb
+    .getUserPosts(req.user.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error getting users post"
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+// Delete user by id
+router.delete("/:id", (req, res) => {
+  userDb
+    .remove(req.user.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error deleting user"
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+// Edit user by id
+router.put("/:id", validateUserId, (req, res) => {
+  userDb
+    .update(req.user.id, req.user.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error updating user"
+      });
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  userDb.getById(req.params.id).then(user => {
+    user
+      ? res.status(200).json({ message: "User ID is valid" })
+      : res.status(400).json({ message: "Invalid user ID" });
+  });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  req.body
+    ? res.status(200).json({ message: "User is valid" })
+    : res.status(400).json({
+        message: "Invalid User"
+      });
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  req.body
+    ? res.status(200).json({ message: "Post is valid" })
+    : res.status(400).json({ message: "Invalid post" });
 }
 
 module.exports = router;
